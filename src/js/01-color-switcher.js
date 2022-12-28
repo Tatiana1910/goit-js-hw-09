ae6efd98d4717c2aa6fa52fa1eb0f19fd57c03aa
Tatiana Kramarenko <tanya19101996@gmail.com>
2022-12-28 11:08:15 +0200

    task-1_done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+startBtn.addEventListener('click', onStartChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+  }, 1000);
+}
+
+function onStopChangeColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
