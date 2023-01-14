eea353d0abc4783dc8fc13fec80111c526370535
Tatiana1910 <110292456+Tatiana1910@users.noreply.github.com>
2023-01-14 12:03:37 +0000

    Deploying to gh-pages from @ Tatiana1910/goit-js-hw-09@507ae81e87c4a815af2a9a696de1f49712051d96 🚀

---
+++ 01-color-switcher.5dd96b74.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.5dd96b74.js.map
